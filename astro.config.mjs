74fb818aeca34f159f025da251c283ef7c2c9b9a
RogelioLB <rogelio20052011@gmail.com>
2023-06-11 01:18:35 -0700

    SSR Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), react(), tailwind()],
-  output:'server',
-  site:'http://localhost:3000'
+  output: 'server',
+  site: 'http://localhost:3000',
+  adapter: netlify()
 });
