5e42e41f708a170c617a14af68110088b050cfea
RogelioLB <rogelio20052011@gmail.com>
2023-07-08 14:48:29 -0700

    Implementing Share Option

--- src/components/ButtonShare.tsx
+++ src/components/ButtonShare.tsx
@@ -1,9 +1,9 @@
 import { BiShare } from "react-icons/bi/index";
 
-export default function ButtonShare(){
+export default function ButtonShare({title}:{title:string}){
     const handleClick = async () => {
         if(navigator.share)
-        await navigator.share({text:"A"})
+        await navigator.share({text:"Rapido, echa un vistazo a este articulo.",title, url: location.href})
         else alert("Tu navegador no soporta compartir")
     }
 
